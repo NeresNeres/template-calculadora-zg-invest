0534b6d3d748b8914ec7e58b29b734afd36a9308
NeresNeres <neres.s@hotmail.com>
2026-09-08 16:30:46 -0300

    Teste Sidenilson Neres dos Santos CalculadoraDeRendimentosZgInvest
    Desafio Técnico Sistema de Gestão de Ativos
    - implemetação da class CalculadoraDeRendimentosZgInvest para demostrar as transações

--- src/main/java/br/com/zginvest/calculadora/CalculadoraDeRendimentosZgInvest.java
+++ src/main/java/br/com/zginvest/calculadora/CalculadoraDeRendimentosZgInvest.java
@@ -20,6 +20,53 @@ public class CalculadoraDeRendimentosZgInvest {
 	}
 
 	public PosicaoCarteira calcularPosicaoEm(LocalDate data) {
-		return new PosicaoCarteira(data, -1, BigDecimal.ZERO, BigDecimal.ZERO);
+
+		BigDecimal precoFechamento = this.precosFechamento.get(data);
+
+		if (precoFechamento == null) {
+			throw new IllegalArgumentException("Não existe preço de fechamento para a data " + data);
+		}
+
+		long quantidadeAcoes = 0;
+		BigDecimal valorInvestido = BigDecimal.ZERO;
+
+		for (Negociacao negociacao : this.negociacoes) {
+
+			if (negociacao.data().isAfter(data)) {
+				break;
+			}
+
+			BigDecimal valorOperacao = negociacao.preco().multiply(BigDecimal.valueOf(negociacao.quantidade()));
+
+			if (negociacao.tipo() == OperacaoNegociacao.COMPRA) {
+
+				quantidadeAcoes += negociacao.quantidade();
+				valorInvestido = valorInvestido.add(valorOperacao);
+
+			} else {
+
+				quantidadeAcoes -= negociacao.quantidade();
+				valorInvestido = valorInvestido.subtract(valorOperacao);
+			}
+		}
+
+		BigDecimal saldoAtual = precoFechamento
+				.multiply(BigDecimal.valueOf(quantidadeAcoes))
+				.setScale(2, java.math.RoundingMode.HALF_UP);
+
+		BigDecimal rendimentoPercentual = saldoAtual
+				.subtract(valorInvestido)
+				.multiply(BigDecimal.valueOf(100))
+				.divide(valorInvestido, 2, java.math.RoundingMode.HALF_UP);
+
+		if (valorInvestido.compareTo(BigDecimal.ZERO) == 0) {
+			rendimentoPercentual = BigDecimal.ZERO;
+		}
+
+		return new PosicaoCarteira(
+				data,
+				quantidadeAcoes,
+				saldoAtual,
+				rendimentoPercentual);
 	}
 }
